3572457e4dad2d2d1c0b4b610445fb4862f41ae8
SJTU-lyx <sjtulyx2003@sjtu.edu.cn>
2025-01-18 14:37:40 +0800

    fix bug in the display of progressbar

--- compresslab/utils/log.py
+++ compresslab/utils/log.py
@@ -1,5 +1,93 @@
 import logging
 import colorlog
+import sys
+from logging import Handler, getLevelName
+from rich.console import Console
+from rich.file_proxy import FileProxy
+
+class StreamHandler(Handler):
+    """
+    A handler class which writes logging records, appropriately formatted,
+    to a stream. Note that this class does not close the stream, as
+    sys.stdout or sys.stderr may be used.
+    """
+
+    terminator = '\n'
+
+    def __init__(self, stream=None):
+        """
+        Initialize the handler.
+
+        If stream is not specified, sys.stderr is used.
+        """
+        Handler.__init__(self)
+        if stream is None:
+            stream = sys.stderr
+        self.stream = stream
+
+    def flush(self):
+        """
+        Flushes the stream.
+        """
+        self.acquire()
+        try:
+            if self.stream and hasattr(self.stream, "flush"):
+                self.stream.flush()
+        finally:
+            self.release()
+
+    def emit(self, record):
+        """
+        Emit a record.
+
+        If a formatter is specified, it is used to format the record.
+        The record is then written to the stream with a trailing newline.  If
+        exception information is present, it is formatted using
+        traceback.print_exception and appended to the stream.  If the stream
+        has an 'encoding' attribute, it is used to determine how to do the
+        output to the stream.
+        """
+        try:
+            msg = self.format(record)
+            stream = self.stream
+            # FIXME: the implementation of StreamHandler in `logging` will lead to the repeat of the progress bar, use sys.stderr here can avoid this problem
+            # stream.write(msg + self.terminator)
+            sys.stderr.write(msg + self.terminator)
+            self.flush()
+        except RecursionError:  # See issue 36272
+            raise
+        except Exception:
+            self.handleError(record)
+
+    def setStream(self, stream):
+        """
+        Sets the StreamHandler's stream to the specified value,
+        if it is different.
+
+        Returns the old stream, if the stream was changed, or None
+        if it wasn't.
+        """
+        if stream is self.stream:
+            result = None
+        else:
+            result = self.stream
+            self.acquire()
+            try:
+                self.flush()
+                self.stream = stream
+            finally:
+                self.release()
+        return result
+
+    def __repr__(self):
+        level = getLevelName(self.level)
+        name = getattr(self.stream, 'name', '')
+        #  bpo-36015: name can be an int
+        name = str(name)
+        if name:
+            name += ' '
+        return '<%s %s(%s)>' % (self.__class__.__name__, name, level)
+
 
 # modify the color of the log here
 colors_config = {
@@ -14,11 +102,12 @@ colors_config = {
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-console_handler = logging.StreamHandler()
+console_handler = StreamHandler()
 console_formatter = colorlog.ColoredFormatter(
     fmt='%(log_color)s[%(asctime)s.%(msecs)03d] %(filename)s -> %(funcName)s line:%(lineno)d [%(levelname)s] : %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
-    log_colors=colors_config
+    log_colors=colors_config,
+    stream=sys.stderr
 )
 console_handler.setFormatter(console_formatter)
 
@@ -26,20 +115,3 @@ if not logger.handlers:
     logger.addHandler(console_handler)
     
 console_handler.close()
-
-
-def debug(msg, *args, **kwargs):
-    logging.debug(msg, *args, **kwargs)
-
-def info(msg, *args, **kwargs):
-    logging.info(msg, *args, **kwargs)
-
-def warning(msg, *args, **kwargs):
-    logging.warning(msg, *args, **kwargs)
-
-def error(msg, *args, **kwargs):
-    logging.error(msg, *args, **kwargs)
-
-def critical(msg, *args, **kwargs):
-    logging.critical(msg, *args, **kwargs)
-
