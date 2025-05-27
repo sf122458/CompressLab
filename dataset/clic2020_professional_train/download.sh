b762b6e9f039d6d7f54d634bf67566d9d3207b12
sjtulyx <sjtulyx2003@sjtu.edu.cn>
2025-05-27 11:07:19 +0800

    add clic2020 dataset

--- dataset/clic2020_professional_train/download.sh
+++ dataset/clic2020_professional_train/download.sh
@@ -1,3 +1,4 @@
 wget https://data.vision.ee.ethz.ch/cvl/clic/professional_train_2020.zip
 unzip professional_train_2020.zip -d ./
 rm professional_train_2020.zip
+rm -rf __MACOSX
---
+++ dataset/clic2020_professional_valid/download.sh
@@ -0,0 +1,4 @@
+wget https://data.vision.ee.ethz.ch/cvl/clic/professional_valid_2020.zip
+unzip professional_valid_2020.zip -d ./
+rm professional_valid_2020.zip
+rm -rf __MACOSX
